7f26696b2b136a3d47227810c998dedccef9dda0
Talal Ibrahim <ibrahim@ebi.ac.uk>
2020-09-10 16:34:03 +0500

    Data access layer structure and logic defined

---
+++ src/main/java/com/dtol/platform/es/repository/Impl/BioSampleRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.dtol.platform.es.repository.Impl;
+
+import com.dtol.platform.es.mapping.BioSample;
+import com.dtol.platform.es.repository.BioSampleRepository;
+import org.elasticsearch.index.query.Operator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+
+@Repository
+public abstract class BioSampleRepositoryImpl implements BioSampleRepository {
+
+    @Autowired
+    private ElasticsearchOperations elasticsearchOperations;
+
+    public BioSample save(BioSample bioSample) {
+        IndexCoordinates indexCoordinates = elasticsearchOperations.getIndexCoordinatesFor(BioSample.class);
+        IndexQuery indexQuery = new IndexQueryBuilder()
+                .withId(UUID.randomUUID().toString())
+                .withObject(bioSample)
+                .build();
+        String documentId = elasticsearchOperations.index(indexQuery, indexCoordinates);
+        return bioSample;
+    }
+
+    public BioSample findBioSampleByAccession(String accession) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery("accession",accession).operator(Operator.AND))
+                .build();
+        SearchHits<BioSample> bioSample = elasticsearchOperations
+                .search(searchQuery, BioSample.class, IndexCoordinates.of("dtol"));
+
+        if(bioSample.getTotalHits() > 0) {
+            return bioSample.getSearchHit(0).getContent();
+        }
+        else {
+            return new BioSample();
+        }
+
+
+    }
+}
