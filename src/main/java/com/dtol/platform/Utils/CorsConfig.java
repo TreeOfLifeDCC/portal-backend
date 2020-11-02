a0adf4e85fc7da27708bd02835fd5bb7854a12ac
Talal Ibrahim <ibrahim@ebi.ac.uk>
2020-11-02 14:15:28 +0000

    CORS allowed methods defined

--- src/main/java/com/dtol/platform/Utils/CorsConfig.java
+++ src/main/java/com/dtol/platform/Utils/CorsConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**");
+            registry.addMapping("/**")
+                    .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
         }
     }
