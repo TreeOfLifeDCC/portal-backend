8d534e9d17d65a4abbf1507439bb654e20633afe
Raheela <32760007+Raheela1024@users.noreply.github.com>
2022-09-07 15:02:26 +0100

    Task ABC-171 api updated for filters.

--- src/main/java/com/dtol/platform/es/service/RootSampleService.java
+++ src/main/java/com/dtol/platform/es/service/RootSampleService.java
@@ -46,7 +46,7 @@ public interface RootSampleService {
 
     public ByteArrayInputStream csvDownload(Optional<String> search, Optional<String> filter, Optional<String> from, Optional<String> size, Optional<String> sortColumn, Optional<String> sortOrder, Optional<String> taxonomyFilter) throws ParseException, IOException;
 
-    public JSONArray getGisData() throws ParseException;
+    public String getGisData(String search, Optional<String> filter) throws ParseException;
 
     ByteArrayInputStream getDataFiles(Optional<String> search, Optional<String> filter,Optional<String> from, Optional<String> size, Optional<String> sortColumn, Optional<String> sortOrder, Optional<String> taxonomyFilter,String downloadOption) throws ParseException, IOException;
 
